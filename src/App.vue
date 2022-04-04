920d8bf92d1315260a613d20c36f6fffb6754e7a
市来玲果 <ichikireika@ichikireikanoMacBook-Air.local>
2022-04-04 20:46:50 +0900

    練習

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
+      </div>
+    </form>
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다
+    </div>
+    <div 
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input 
+            class="form-check-input" 
+            type="checkbox"
+            v-model="todo.completed"
+          >
+          <label 
+            class="form-check-label"
+            :class="{ todo: todo.completed }"
+          >
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+          <button 
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const todo = ref('');
+    const todos = ref([]);
+    const hasError = ref(false);
+
+    const onSubmit = () => {
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        });
+        hasError.value = false;
+        todo.value = '';
+      }
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+      hasError,
+      deleteTodo,
+    };
+  }
+}
+</script>
+
+<style>
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
+</style>
